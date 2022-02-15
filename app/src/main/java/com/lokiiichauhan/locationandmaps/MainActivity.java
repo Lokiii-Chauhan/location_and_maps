36aa3bc9e0b204828cbd0b6dd018b0fa04315bb8
lokii <lokiiichauhan@gmail.com>
2022-02-16 01:32:39 +0530

    permissions implemented

--- app/src/main/java/com/lokiiichauhan/locationandmaps/MainActivity.java
+++ app/src/main/java/com/lokiiichauhan/locationandmaps/MainActivity.java
@@ -1,14 +1,225 @@
 package com.lokiiichauhan.locationandmaps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    public static final long FASTEST_INTERVAL = 5000L;
+    private static final long UPDATE_INTERVAL = 5000L;
+    private static final int ALL_PERMISSION_RESULT = 111;
+    private GoogleApiClient client;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private ArrayList<String> permissionToRequest, permissions, permissionsRejected;
+    private TextView locationTextView;
+    private LocationRequest locationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        permissions = new ArrayList<>();
+        permissionsRejected = new ArrayList<>();
+
+        locationTextView = findViewById(R.id.location_text_view);
+
+        fusedLocationProviderClient = LocationServices
+                .getFusedLocationProviderClient(this);
+
+        //let's add permission we need to request location of users
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        permissionToRequest = permissionToRequest(permissions);
+
+        client = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+    private ArrayList<String> permissionToRequest(ArrayList<String> wantedPermissions) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String perm : wantedPermissions) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+        return result;
+    }
+
+    private boolean hasPermission(String perm) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(perm) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        checkPlayServices();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (client != null && client.isConnected()) {
+            LocationServices.getFusedLocationProviderClient(this)
+                    .removeLocationUpdates(new LocationCallback() {
+
+
+                    });
+
+            client.disconnect();
+        }
+
+    }
+
+    private void checkPlayServices() {
+        int error_code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+        if (error_code != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = GoogleApiAvailability.getInstance()
+                    .getErrorDialog(this, error_code, error_code, dialog -> {
+                        Toast.makeText(this, "No Services", Toast.LENGTH_SHORT).show();
+                        finish();
+                    });
+            errorDialog.show();
+        } else {
+            Toast.makeText(this, "No Services", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    locationTextView.setText("Lat:" + location.getLatitude() +
+                            " Lon: " + location.getLongitude());
+                }
+            }
+        });
+
+        startLocationUpdates();
+
+    }
+
+    private void startLocationUpdates() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "You Need to enable permissions to display location", Toast.LENGTH_LONG).show();
+        }
+
+        LocationServices.getFusedLocationProviderClient(this)
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(@NonNull LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        if (locationResult != null) {
+                            Location location = locationResult.getLastLocation();
+                            locationTextView.setText(String.format("Lat: %s Lon: %s", location.getLongitude(), location.getLongitude()));
+                        }
+                    }
+
+                    @Override
+                    public void onLocationAvailability(@NonNull LocationAvailability locationAvailability) {
+                        super.onLocationAvailability(locationAvailability);
+
+                    }
+                }, null);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case ALL_PERMISSION_RESULT:
+                for (String perm : permissionToRequest) {
+                    if (!hasPermission(perm)) {
+                        permissionsRejected.add(perm);
+                    }
+                }
+                if (permissionsRejected.size() > 0) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                            new AlertDialog.Builder(this).setMessage("There are mandatory to get location")
+                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(permissionsRejected
+                                                        .toArray(new String[permissionsRejected
+                                                                .size()]), ALL_PERMISSION_RESULT);
+                                            }
+                                        }
+                                    }).setNegativeButton("Cancel", null).show();
+                        }
+                    }
+                } else if (client != null) {
+                    client.connect();
+                }
+        }
     }
 }
